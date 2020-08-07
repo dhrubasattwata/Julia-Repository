e1fb6c3622c123b6c543b7605da3be0a11e5862d
Dhrubasattwata Roy Choudhury <dhrubasattwata@gmail.com>
2020-08-07 20:49:34 +0530

    Add files via upload
    Basic Julia Operators

---
+++ Julia_example1.jl
@@ -0,0 +1,31 @@
+# BASICS ==============================================================================================================
+
+versioninfo()
+
+# Make your life easier by keeping everything in a script. Julia scripts typically have a ".jl" suffix.
+# You can execute the contents of a script by using include() or require(), where the latter
+# will only execute the script once.
+
+# Comments
+#=
+Longer comments between "#=" and "=#".
+... done!
+=#
+
+# Julia is a fancy calculator (with an absurd number of bells and whistles!).
+
+7 + 2
+sqrt(729)
+4pi
+90 / 2
+2 \ 4
+div(97, 2)
+13 % 2
+
+# Retrieve previous result
+ans
+
+# GETTING HELP & INFORMATION ------------------------------------------------------------------------------------------
+
+# Hit '?' and then type a function name.
+apropos("plot")
